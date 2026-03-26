4880b6b287edf3ede3458f67ea01098a1cfc5e4b
Angel <etti.joseangel@gmail.com>
2026-03-25 20:54:57 -0600

    Metodo uploadFile añadido e interfaz UploadResponse para la integracion de Azure

--- frontend/next-step/src/app/services/data.ts
+++ frontend/next-step/src/app/services/data.ts
@@ -1,34 +1,62 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { delay, Observable, of } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+export interface UploadResponse{
+file_url: string;
+input_type: string;
+blob_name: string;
+}
 
 @Injectable({
   providedIn: 'root',
 })
+
 export class Data {
 
-  //Backend URL dentro de las comillas simples
-  private readonly API_URL = '';
+  //URL base
+  private readonly API_URL = environment.api_URL;
 
   constructor(private http: HttpClient){}
 
-  //Simular envío de archivos
-  uploadFile(file: File, type: string): Observable<any>{
-    console.log(`Subiendo archivo de tipo: ${type}`, file.name);
+  // //Simular envío de archivos
+  // uploadFile(file: File, type: string): Observable<any>{
+  //   console.log(`Subiendo archivo de tipo: ${type}`, file.name);
 
-    //Simulamos respuestas del servidor tras 2 segundos
-    return of({
-      status: 'success',
-      message: 'Archivo procesado correctamente',
-      extrated_data: { temp_limit: '40°C', sensor_id: 'RS-99' }
-    }).pipe(delay(2000));
+  //   //Simulamos respuestas del servidor tras 2 segundos
+  //   return of({
+  //     status: 'success',
+  //     message: 'Archivo procesado correctamente',
+  //     extrated_data: { temp_limit: '40°C', sensor_id: 'RS-99' }
+  //   }).pipe(delay(2000));
 
-    // Respuesta REAL: return this.http.post(`${this.API_URL}/analyze`, { file, type });
+  //   // Respuesta REAL: return this.http.post(`${this.API_URL}/analyze`, { file, type });
+  // }
+
+  //** Salud del sistema
+  checkHealth(): Observable<any> {
+    return this.http.get(`${this.API_URL}/${environment.health}`);
   }
 
-  // 2. Simular Chat con la IA
+  //** Subida de archivos
+  uploadFile(file: File, type: string): Observable<UploadResponse> {
+    console.log(`Iniciando subida REAL a Azure. Tipo: ${type}`, file.name);
+
+    // FormData creado
+    const formData = new FormData();
+
+    // Agregamos el archivo y el tipo
+    formData.append('file', file); 
+    formData.append('type', type);
+
+    // Hacemos la peticion POST a la URL 
+    return this.http.post<UploadResponse>(`${this.API_URL}/${environment.upload}`, formData);
+  }
+
+  // ** Chat con la IA
   sendMessage(message: string): Observable<any> {
-    console.log('Enviando mensaje', message);
+    console.log('Enviando mensaje al agente', message);
 
     //MOCK: Respuesta automatica de la IA
     return of({
@@ -36,7 +64,13 @@ export class Data {
       timestamp: new Date()
     }).pipe(delay(1500));
 
-    // Respuesta REAL: return this.http.post(`${this.API_URL}/chat`, { message });
+    // --- RESPUESTA REAL (Descomentar al conectar el Backend) ---
+    /*
+    return this.http.post(`${this.API_URL}/$analyze_endpoint`, { 
+      prompt: message,
+      session_id: 'hackathon-001' // Opcional, según pida tu backend
+    });
+    */
   }
 
 
