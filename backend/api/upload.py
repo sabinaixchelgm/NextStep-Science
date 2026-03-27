1496cfaf2a770a87bcb3cfa41efa9ddc18d87e7d
Sabina Ixchel Garcia Mendoza <89890640+sabinaixchelgm@users.noreply.github.com>
2026-03-27 03:51:21 -0400

    Update upload.py
    borrar comments debug

--- backend/api/upload.py
+++ backend/api/upload.py
@@ -10,11 +10,7 @@ bp = func.Blueprint()
 
 @bp.route(route="upload", methods=["POST"])
 def upload_endpoint(req: func.HttpRequest) -> func.HttpResponse:
-    # --- DEBUG BLOQUE ---
-    logging.info(f"Content-Type entrante: {req.headers.get('Content-Type')}")
-    logging.info(f"Body size: {len(req.get_body())} bytes")
-    logging.info(f"Files detectados: {req.files.keys()}")
-    # -------------------
+
     files = req.files
     if not files or not files.getlist('file'):
         return func.HttpResponse(json.dumps({"error": "Missing file"}), status_code=400, mimetype="application/json")
