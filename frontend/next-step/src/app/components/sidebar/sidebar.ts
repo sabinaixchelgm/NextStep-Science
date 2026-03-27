b91b7c54f0a8cf5840312a2e43a5db2cc9adead5
Angel <etti.joseangel@gmail.com>
2026-03-27 00:22:01 -0600

    Boton de hamburguesa implementado en <aside>

--- frontend/next-step/src/app/components/sidebar/sidebar.ts
+++ frontend/next-step/src/app/components/sidebar/sidebar.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
 export class Sidebar {
+
+  public isCollapsed: boolean = false;
+
+  toggleSidebar() {
+  this.isCollapsed = !this.isCollapsed;
+  }
 
 }
